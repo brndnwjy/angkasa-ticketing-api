65f835c96d2cef19d2837fd8bc2f5599c0ddc1a1
Brandon Wijaya <brndnwjy8@gmail.com>
2022-10-21 21:09:23 +0700

    Update file db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,8 @@
+-- Database
 CREATE DATABASE angkasa
 
+
+-- Users
 CREATE TABLE users (
     user_id UUID  PRIMARY KEY NOT NULL, 
     username VARCHAR(64) NOT NULL,
@@ -14,6 +17,11 @@ CREATE TABLE users (
     updated_at TIMESTAMP
 );
 
+INSERT INTO users (user_id, username, email, phone, city, address, postcode, password)
+VALUES ('8b65d7af-91c2-478f-8655-ddaabeb3fa80', 'Brandon', 'brandon@mail.com', '08563344252537', 'Jakarta', 'Walet Street Number 100', '11800', 'password');
+
+
+-- Admins
 CREATE TABLE admins (
     admin_id UUID  PRIMARY KEY NOT NULL, 
     username VARCHAR(64) NOT NULL,
@@ -23,12 +31,19 @@ CREATE TABLE admins (
     updated_at TIMESTAMP
 );
 
+
+-- Airlines
 CREATE TABLE airlines (
     airline_id UUID PRIMARY KEY NOT NULL,
     name VARCHAR (256),
     logo VARCHAR
 );
 
+INSERT INTO airlines (airline_id, name)
+VALUES ('cb321f1c-7f85-4b40-8f35-8829db0bc702', 'Garuda');
+
+
+-- Flights
 CREATE TABLE IF NOT EXISTS flights (
     flight_id UUID PRIMARY KEY NOT NULL,
     arrival_country VARCHAR(50) NOT NULL,
@@ -37,22 +52,37 @@ CREATE TABLE IF NOT EXISTS flights (
     departure_city VARCHAR(50) NOT NULL,
     arrival_time VARCHAR NOT NULL,
     departure_time VARCHAR NOT NULL,
-    price VARCHAR NOT NULL,
+    price INTEGER NOT NULL,
     airline_id UUID REFERENCES airlines(airline_id),
     terminal VARCHAR NOT NULL,
     gate VARCHAR NOT NULL,
     wifi BOOLEAN NOT NULL,
-    bagasi BOOLEAN NOT NULL,
+    luggage BOOLEAN NOT NULL,
     lunch BOOLEAN NOT NULL,
-    created_at TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP WITH TIMEZONE,
+    created_at TIMESTAMP NOT NULL DEFAULT Now(),
+    updated_at TIMESTAMP
 );
 
+INSERT INTO flights (flight_id, arrival_country, arrival_city, departure_country, departure_city,
+arrival_time, departure_time, price, airline_id, terminal, gate, wifi, luggage, lunch)
+VALUES ('00f4fe0d-61da-40aa-a7ab-75a0637c5d86', 'SGP', 'Singapore', 'IDN', 'Jakarta', '12:00', 
+'10:00', '1500000', 'cb321f1c-7f85-4b40-8f35-8829db0bc702', 'Soekarno', 'A', true, true, true );
+
+
+-- Bookings
 CREATE TABLE IF NOT EXISTS bookings (
     booking_id UUID PRIMARY KEY NOT NULL,
     user_id UUID REFERENCES users(user_id),
     flight_id UUID REFERENCES flights(flight_id),
-    status VARCHAR NOT NULL,
-    created_at TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP WITH TIMEZONE,
+    cp_name VARCHAR(64) NOT NULL,
+    cp_email VARCHAR (64) NOT NULL,
+    cp_phone VARCHAR(16) NOT NULL,
+    passenger_title VARCHAR(64) NOT NULL,
+    passenger_name VARCHAR (64) NOT NULL,
+    passenger_nationality VARCHAR(16) NOT NULL,
+    travel_insurance BOOLEAN DEFAULT false,
+    payment_status BOOLEAN DEFAULT false,
+    total_payment INTEGER,
+    created_at TIMESTAMP NOT NULL DEFAULT Now(),
+    updated_at TIMESTAMP
 );
