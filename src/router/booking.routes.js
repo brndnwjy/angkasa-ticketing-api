e57baee220599b51238d3f31490b70cf616bc3e7
Brandon Wijaya <brndnwjy8@gmail.com>
2022-10-23 00:43:43 +0700

    update develop branch, undo clearance

--- src/router/booking.routes.js
+++ src/router/booking.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { getAllBooking, getBookingDetail, createBooking, updateBooking, cancelBooking } = require("../controller/user/booking.controller");
 const router = express.Router();
 
-// router
-// .get()
-// .post()
-// .put()
-// .delete()
+router
+.get("/", getAllBooking)
+.get("/:id", getBookingDetail)
+.post("/", createBooking)
+.put("/:id", updateBooking)
+.delete("/:id", cancelBooking);
 
 module.exports = router;
--- src/router/flight.routes.js
+++ src/router/flight.routes.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const {listFlight, insertFlight, updateFlight, deleteFlight} = require('../controller/user/flight.controller')
+
+// middleware
+
+
 // router
 // .get()
 // .post()
 // .put()
 // .delete()
 
+router.get('/', (req, res) => {
+    const data = 'test routes';
+    res.json(data);
+})
+
+router
+    .get('/list', listFlight)
+    .post('/insert', insertFlight)
+    .put('/update/:id', updateFlight)
+    .delete('/delete/:id', deleteFlight);
 module.exports = router;
