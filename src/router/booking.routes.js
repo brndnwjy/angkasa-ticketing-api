e33a8cbd4b599ecf95d390466f1a6693eb41138d
Brandon Wijaya <brndnwjy8@gmail.com>
2022-10-23 00:29:12 +0700

    clean non-airline route on branch

--- src/router/booking.routes.js
+++ src/router/booking.routes.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const { getAllBooking, getBookingDetail, createBooking, updateBooking, cancelBooking } = require("../controller/user/booking.controller");
 const router = express.Router();
 
-router
-.get("/", getAllBooking)
-.get("/:id", getBookingDetail)
-.post("/", createBooking)
-.put("/:id", updateBooking)
-.delete("/:id", cancelBooking);
+// router
+// .get()
+// .post()
+// .put()
+// .delete()
 
 module.exports = router;
