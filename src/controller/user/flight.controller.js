156eaa542b2ef1f800adbdd7e74753c4e28a31a3
ainurcoding <ainurridwan98@gmail.com>
2022-10-23 14:14:22 +0700

    add models

--- src/controller/user/flight.controller.js
+++ src/controller/user/flight.controller.js
@@ -81,36 +81,33 @@ const flightModelController = {
            })
             
     },
-    // insertFlight: (req, res) => {
-    //     const { arrival_country, arrival_city,
-    //         departure_country, departure_city,
-    //         arrival_time, departure_time,
-    //         price, terminal, gate,
-    //         wifi, baggage, lunch } = req.body;
-    //     const data = {
-    //         arrival_country, arrival_city,
-    //         departure_country, departure_city,
-    //         arrival_time, departure_time, price,
-    //         terminal, gate, wifi, baggage, lunch
-    //     };
+    flightWithFilter: (req, res) => {
+        // const search = req.query.search || "";
+        // const sortBy = req.query.sortby || "flight_id";
+        // const sortOrder = req.query.order || "asc";
+        // console.log(search +" "+sortBy)
+        // flightModel
+        //     .flightWithFilter(search, sortBy, sortOrder)
+        //     .then((results) => {
+        //         // success(res, data, "success", "data has been update"); 
+        //         res.json(results);
+        //    }).catch((err) => {
+        //         res.json(err)
+        //    })
 
-    //     flightModel
-    //         .insertFlight(data)
-    //         .then((results) => {
-    //             console.log(results);
-    //             const flight = results.rows[0];
-    //             if (results.rowCount > 1) {
-    //                 success(res, {
-    //                     data: flight,
-    //                 }, "success", "flight data has been entered");
-    //             } else {
-    //                 failed(res, null, "failed", "failed to add data")
-    //             }
-
-    //         }).catch((err) => {
-    //             failed(res, err.message, "failed", "internal server error");
-    //         })
-    // },
+        // const search = 'depok';
+        // const sortBy = 'flight_id';
+        const search = req.query.search || "";
+        const sortBy = req.query.sortby || "arrival_country";
+        flightModel 
+            .flightWithFilter(search, sortBy)
+            .then((results) => {
+                res.json(results);
+            }).catch((err) => {
+                // res.json(err);
+                failed(res, err.message, "failed", "internal server error"); 
+            });
+    },
 }
 
 module.exports = flightModelController;
--- src/router/flight.routes.js
+++ src/router/flight.routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {listFlight, insertFlight, updateFlight, deleteFlight} = require('../controller/user/flight.controller')
+const {
+    listFlight,
+    insertFlight,
+    updateFlight,
+    deleteFlight,
+    flightWithFilter
+} = require('../controller/user/flight.controller')
 
 // middleware
 
@@ -12,12 +18,10 @@ const {listFlight, insertFlight, updateFlight, deleteFlight} = require('../contr
 // .put()
 // .delete()
 
-router.get('/', (req, res) => {
-    const data = 'test routes';
-    res.json(data);
-})
+
 
 router
+    .get('/', flightWithFilter)
     .get('/list', listFlight)
     .post('/insert', insertFlight)
     .put('/update/:id', updateFlight)
